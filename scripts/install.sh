05a894a4f66b6999123b2d66bad29e552566e8ac
Fadri Furrer <ffurrer@gmail.com>
2016-12-20 17:33:45 +0100

    remove echos

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,21 +3,15 @@
 SRC=$1
 SRC2=$2
 DEST_INC=$3
-echo "#####################"
-echo $(find $SRC -name *.h)
-echo "#####################"
-echo $(find $SRC2 -name *.h)
 
 # Copy all headers to devel/include
 for i in $(find $SRC -name *.h); do
-  echo ${i}
 	F=$DEST_INC/${i##$SRC}
 	mkdir -p $(dirname $F)
 	cp $i $F
 done
 
 for i in $(find $SRC2 -name *.h); do
-  echo ${i}
 	F=$DEST_INC/${i##$SRC2}
 	mkdir -p $(dirname $F)
 	cp $i $F
